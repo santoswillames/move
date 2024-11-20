94bef8226b55c6a955ec4d23e7af8352f1228991
willamessantos <willamessantos@live.com>
2024-11-20 15:46:02 -0300

    start server

---
+++ api/src/http/routes.ts
@@ -0,0 +1,28 @@
+import type { FastifyInstance } from "fastify";
+
+export async function appRoutes(app: FastifyInstance) {
+  /* app.post("/ride/estimate", () =>
+    console.log({
+      customer_id: "string",
+      origin: "string",
+      destination: "string",
+    })
+  );
+
+  app.patch("ride/confirm", () =>
+    console.log({
+      customer_id: "string",
+      origin: "string",
+      destination: "string",
+      distance: "number",
+      duration: "string",
+      driver: {
+        id: "number",
+        name: "string",
+      },
+      value: "number",
+    })
+  );
+  app.get("/ride/:customer_id") // deve conter a query driver_id 
+  */
+}
---
+++ api/src/server.ts
@@ -0,0 +1,15 @@
+import { app } from "./app";
+import { env } from "./env";
+
+app.get("/", (_, reply) => {
+  reply.send({ hello: "world" });
+});
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("🚀 HTTP server running");
+  });
